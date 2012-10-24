e8bc9e86a06087dec1a7518b0b142b03abc55914
Jake McCrary <jake@jakemccrary.com>
2012-10-23 21:09:51 -0500

    mark functions as private

--- src/jakemcc/clojure_gntp/gntp.clj
+++ src/jakemcc/clojure_gntp/gntp.clj
@@ -11,21 +11,20 @@
                                  :appname "clojure-gntp"
                                  :password nil
                                  :app "clojure-gntp"
-                                 :title "clojure-gntp"
                                  :notification "clojure-gntp-notify"})
 
 (defmulti serialize-value type)
 (defmethod serialize-value :default [x] (str x))
 (defmethod serialize-value java.lang.Boolean [x] (if x "True" "False"))
 
-(defn header [name value]
+(defn- header [name value]
   (str name ": " (serialize-value value)))
 
-(defn main-header [type]
+(defn- main-header [type]
   (str "GNTP/" version " " type " " encryption " "))
-(defn ending [] ["" ""])
+(defn- ending [] ["" ""])
 
-(defn send-message [out entries]
+(defn- send-message [out entries]
   (let [message (apply str
                        (interpose separator
                                   entries))]
@@ -38,7 +37,7 @@
 (defn- read-all [in]
   (slurp in))
 
-(defn register [app out in]
+(defn- register [app out in]
   (send-message out
                 (list* (main-header "REGISTER")
                        (application-name (:app  app))
@@ -51,25 +50,25 @@
   (read-all in)
   nil)
 
-(defn notify [app message out in]
+(defn- notify [app title message out in]
   (send-message out
                 (list* (main-header "NOTIFY")
                        (application-name (:app app))
                        (header "Notification-Name" (:notification app))
-                       (header "Notification-Title" (:title app))
+                       (header "Notification-Title" title)
                        (header "Notification-Text" message)
                        (ending)))
   (read-all in)
   nil)
 
-(defn with-socket [f]
+(defn- with-socket [f]
   (with-open [socket (Socket. (:server default-settings)
                               (:port default-settings))
               out (PrintWriter. (.getOutputStream socket) true)
               in (BufferedReader. (InputStreamReader. (.getInputStream socket)))]
     (f out in)))
 
-(defn message [message & options]
+(defn message [title message & options]
   (let [app (merge default-settings (apply hash-map options))]
     (with-socket (partial register app))
-    (with-socket (partial notify app message))))
+    (with-socket (partial notify app title message))))
