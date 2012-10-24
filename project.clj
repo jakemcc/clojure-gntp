fc13be252eeebd7d385de06d61f421aa606da8d2
Jake McCrary <jake@jakemccrary.com>
2012-10-23 21:31:40 -0500

    Release 0.1.0.

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject clojure-gntp "0.1.0-SNAPSHOT"
+(defproject clojure-gntp "0.1.0"
   :description "Implementation of Growl GNTP"
   :url "https://github.com/jakemcc/clojure-gntp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-autoexpect "0.2.1-SNAPSHOT"]]
+  :lein-release {:deploy-via :clojars}
   :profiles {:dev {:dependencies [[expectations "1.4.10"]]}})
